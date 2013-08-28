1f54777317802603d65ff3cc47c0a557cd13c68f
Blackrush <blackrushx@gmail.com>
2013-08-28 17:47:34 +0200

    remove update shell scripts

--- update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-mkdir -p "$HOME/.vim/autoload"
-mkdir -p "$HOME/.vim/ftplugin"
-cp "${0%/*}/autoload/gocomplete.vim" "$HOME/.vim/autoload"
-cp "${0%/*}/ftplugin/go.vim" "$HOME/.vim/ftplugin"
